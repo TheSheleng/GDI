7f8768328756bd167d593e58991c1a2bed2c54fd
Sele_tu30@student.itstep.org <Sele_tu30@student.itstep.org>
2023-09-08 20:26:48 +0300

    1- 8.9.2023 (dz)

---
+++ 8.9.2023 (dz)/MainForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace _8._9._2023__dz_
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            pictureBox = new PictureBox();
+            timer = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)pictureBox).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            pictureBox.Dock = DockStyle.Fill;
+            pictureBox.Location = new Point(0, 0);
+            pictureBox.Name = "pictureBox";
+            pictureBox.Size = new Size(600, 600);
+            pictureBox.TabIndex = 0;
+            pictureBox.TabStop = false;
+            // 
+            // timer
+            // 
+            timer.Interval = 1000;
+            timer.Tick += timer_Tick;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.OldLace;
+            ClientSize = new Size(600, 600);
+            Controls.Add(pictureBox);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "MainForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Clock";
+            ((System.ComponentModel.ISupportInitialize)pictureBox).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox;
+        private System.Windows.Forms.Timer timer;
+    }
+}
---
+++ 8.9.2023 (dz)/MainForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _8._9._2023__dz_
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            timer.Start();
+        }
+
+        private PointF FractionsInDegrees(int fract, int all)
+        {
+            double angle = fract * 360 * Math.PI / all / 180;
+            return new PointF((float)Math.Sin(angle), (float)Math.Cos(angle));
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            Graphics g = this.pictureBox.CreateGraphics();
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            pictureBox.Invalidate();
+            pictureBox.Refresh();
+
+            int height = this.pictureBox.Height;
+            int width = this.pictureBox.Width;
+
+            PointF center = new PointF(width / 2, height / 2);
+
+            using (Pen pen = new Pen(Color.Black, 10))
+                g.DrawEllipse(pen, 30, 30, width - 60, height - 60);
+
+                TimeOnly time = TimeOnly.FromDateTime(DateTime.Now);
+            PointF pHour = FractionsInDegrees(time.Hour, 12);
+            PointF pMinute = FractionsInDegrees(time.Minute, 60);
+            PointF pSecond = FractionsInDegrees(time.Second, 60);
+
+            using (Pen pen = new Pen(Color.Black, 15))
+                g.DrawLine(pen, center.X, center.Y, center.X + pHour.X * 180, center.Y - pHour.Y * 180);
+
+            using (Pen pen = new Pen(Color.Black, 10))
+                g.DrawLine(pen, center.X, center.Y, center.X + pMinute.X * 240, center.Y - pMinute.Y * 240);
+
+            using (Pen pen = new Pen(Color.Red, 5))
+                g.DrawLine(pen, center.X, center.Y, center.X + pSecond.X * 240, center.Y - pSecond.Y * 240);
+
+            using (SolidBrush brush = new SolidBrush(Color.Black))
+                g.FillEllipse(brush, center.X - 10, center.Y - 10, 20, 20);
+
+            g.Dispose();
+        }
+    }
+}
