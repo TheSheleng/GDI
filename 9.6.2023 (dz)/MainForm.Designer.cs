b33f86fe84435b979a6961bcd5fead203c494022
Sele_tu30@student.itstep.org <Sele_tu30@student.itstep.org>
2023-09-08 04:01:37 +0300

    2 - 9.6.2023 (dz)

---
+++ 9.6.2023 (dz)/MainForm.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace _9._6._2023__dz_
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            trackBar1 = new TrackBar();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(30, 608);
+            label1.Name = "label1";
+            label1.Size = new Size(85, 48);
+            label1.TabIndex = 0;
+            label1.Text = "Y = ";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(107, 611);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(423, 55);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(30, 672);
+            button1.Name = "button1";
+            button1.Size = new Size(500, 55);
+            button1.TabIndex = 2;
+            button1.Text = "Рисовать";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox1.Location = new Point(30, 30);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(500, 500);
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(30, 739);
+            label2.Name = "label2";
+            label2.Size = new Size(0, 25);
+            label2.TabIndex = 4;
+            // 
+            // trackBar1
+            // 
+            trackBar1.Location = new Point(30, 536);
+            trackBar1.Maximum = 20;
+            trackBar1.Minimum = 1;
+            trackBar1.Name = "trackBar1";
+            trackBar1.Size = new Size(500, 69);
+            trackBar1.TabIndex = 5;
+            trackBar1.Value = 1;
+            trackBar1.Scroll += trackBar1_Scroll;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(560, 780);
+            Controls.Add(trackBar1);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "MainForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private TrackBar trackBar1;
+    }
+}
---
+++ 9.6.2023 (dz)/MainForm.cs
@@ -0,0 +1,68 @@
+﻿using Antlr.Runtime;
+using NCalc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _9._6._2023__dz_
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void draw()
+        {
+            string input = textBox1.Text;
+
+            Graphics g = pictureBox1.CreateGraphics();
+            Pen pen = new Pen(Color.Black, 4);
+
+            g.Clear(BackColor);
+            g.DrawLine(pen, 0, 252, 500, 252);
+            g.DrawLine(pen, 252, 0, 252, 500);
+
+            try
+            {
+                for (float x = -250; x < 250; x += 1f)
+                {
+                    Expression expr = new Expression(
+                            input.Replace("X", $"({x.ToString().Replace(',', '.')})")
+                        );
+
+                    double result = Convert.ToDouble(expr.Evaluate());
+
+                    g.DrawEllipse(pen, new RectangleF(
+                        x * trackBar1.Value - 2 + 250, 
+                        -((float)result * trackBar1.Value) - 2 + 250, 
+                        4, 4));
+                }
+            }
+            catch (Exception ex)
+            {
+                label2.Text = ex.Message;
+            }
+
+            pen.Dispose();
+            g.Dispose();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            draw();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            draw();
+        }
+    }
+}
