14109648eb333bffe7b36c53dccd13f1c94c8954
Sele_tu30@student.itstep.org <Sele_tu30@student.itstep.org>
2023-09-08 12:58:23 +0300

    3 - 9.6.2023

--- 9.6.2023 (dz)/MainForm.Designer.cs
+++ 9.6.2023 (dz)/MainForm.Designer.cs
@@ -36,6 +36,17 @@
             trackBar1 = new TrackBar();
             trackBar2 = new TrackBar();
             checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            labelX_0 = new Label();
+            labelX_125 = new Label();
+            labelX_250 = new Label();
+            labelX__125 = new Label();
+            labelX__250 = new Label();
+            labelY__250 = new Label();
+            labelY__125 = new Label();
+            labelY_250 = new Label();
+            labelY_125 = new Label();
+            labelY_0 = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)trackBar2).BeginInit();
@@ -45,7 +56,7 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(30, 718);
+            label1.Location = new Point(30, 759);
             label1.Name = "label1";
             label1.Size = new Size(85, 48);
             label1.TabIndex = 0;
@@ -54,16 +65,16 @@
             // textBox1
             // 
             textBox1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
-            textBox1.Location = new Point(107, 721);
+            textBox1.Location = new Point(107, 762);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(423, 55);
+            textBox1.Size = new Size(482, 55);
             textBox1.TabIndex = 1;
             // 
             // button1
             // 
-            button1.Location = new Point(30, 782);
+            button1.Location = new Point(30, 823);
             button1.Name = "button1";
-            button1.Size = new Size(500, 55);
+            button1.Size = new Size(559, 55);
             button1.TabIndex = 2;
             button1.Text = "Рисовать";
             button1.UseVisualStyleBackColor = true;
@@ -72,7 +83,7 @@
             // pictureBox1
             // 
             pictureBox1.BorderStyle = BorderStyle.FixedSingle;
-            pictureBox1.Location = new Point(30, 30);
+            pictureBox1.Location = new Point(68, 39);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(500, 500);
             pictureBox1.TabIndex = 3;
@@ -82,7 +93,7 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            label2.Location = new Point(30, 849);
+            label2.Location = new Point(30, 890);
             label2.Name = "label2";
             label2.Size = new Size(0, 25);
             label2.TabIndex = 4;
@@ -90,39 +101,164 @@
             // trackBar1
             // 
             trackBar1.LargeChange = 1;
-            trackBar1.Location = new Point(30, 536);
+            trackBar1.Location = new Point(30, 577);
             trackBar1.Maximum = 20;
             trackBar1.Minimum = 1;
             trackBar1.Name = "trackBar1";
-            trackBar1.Size = new Size(500, 69);
+            trackBar1.Size = new Size(559, 69);
             trackBar1.TabIndex = 5;
             trackBar1.Value = 1;
             // 
             // trackBar2
             // 
-            trackBar2.Location = new Point(30, 611);
+            trackBar2.Location = new Point(30, 652);
             trackBar2.Maximum = 100;
             trackBar2.Minimum = 1;
             trackBar2.Name = "trackBar2";
-            trackBar2.Size = new Size(500, 69);
+            trackBar2.Size = new Size(559, 69);
             trackBar2.TabIndex = 6;
             trackBar2.Value = 100;
             // 
             // checkBox1
             // 
             checkBox1.AutoSize = true;
-            checkBox1.Location = new Point(30, 686);
+            checkBox1.Checked = true;
+            checkBox1.CheckState = CheckState.Checked;
+            checkBox1.Location = new Point(30, 727);
             checkBox1.Name = "checkBox1";
             checkBox1.Size = new Size(70, 29);
             checkBox1.TabIndex = 7;
             checkBox1.Text = "Оси";
             checkBox1.UseVisualStyleBackColor = true;
             // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Checked = true;
+            checkBox2.CheckState = CheckState.Checked;
+            checkBox2.Location = new Point(100, 727);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(83, 29);
+            checkBox2.TabIndex = 8;
+            checkBox2.Text = "Сетка";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // labelX_0
+            // 
+            labelX_0.AutoSize = true;
+            labelX_0.Location = new Point(310, 11);
+            labelX_0.Name = "labelX_0";
+            labelX_0.Size = new Size(22, 25);
+            labelX_0.TabIndex = 9;
+            labelX_0.Text = "0";
+            labelX_0.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // labelX_125
+            // 
+            labelX_125.AutoSize = true;
+            labelX_125.Location = new Point(418, 11);
+            labelX_125.Name = "labelX_125";
+            labelX_125.Size = new Size(54, 25);
+            labelX_125.TabIndex = 10;
+            labelX_125.Text = "+125";
+            labelX_125.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // labelX_250
+            // 
+            labelX_250.AutoSize = true;
+            labelX_250.Location = new Point(543, 11);
+            labelX_250.Name = "labelX_250";
+            labelX_250.Size = new Size(54, 25);
+            labelX_250.TabIndex = 11;
+            labelX_250.Text = "+250";
+            labelX_250.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // labelX__125
+            // 
+            labelX__125.AutoSize = true;
+            labelX__125.Location = new Point(168, 11);
+            labelX__125.Name = "labelX__125";
+            labelX__125.Size = new Size(49, 25);
+            labelX__125.TabIndex = 12;
+            labelX__125.Text = "-125";
+            labelX__125.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // labelX__250
+            // 
+            labelX__250.AutoSize = true;
+            labelX__250.Location = new Point(43, 11);
+            labelX__250.Name = "labelX__250";
+            labelX__250.Size = new Size(49, 25);
+            labelX__250.TabIndex = 13;
+            labelX__250.Text = "-250";
+            labelX__250.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // labelY__250
+            // 
+            labelY__250.AutoSize = true;
+            labelY__250.Location = new Point(13, 514);
+            labelY__250.Name = "labelY__250";
+            labelY__250.Size = new Size(49, 25);
+            labelY__250.TabIndex = 18;
+            labelY__250.Text = "-250";
+            labelY__250.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // labelY__125
+            // 
+            labelY__125.AutoSize = true;
+            labelY__125.Location = new Point(13, 404);
+            labelY__125.Name = "labelY__125";
+            labelY__125.Size = new Size(49, 25);
+            labelY__125.TabIndex = 17;
+            labelY__125.Text = "-125";
+            labelY__125.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // labelY_250
+            // 
+            labelY_250.AutoSize = true;
+            labelY_250.Location = new Point(8, 39);
+            labelY_250.Name = "labelY_250";
+            labelY_250.Size = new Size(54, 25);
+            labelY_250.TabIndex = 16;
+            labelY_250.Text = "+250";
+            labelY_250.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // labelY_125
+            // 
+            labelY_125.AutoSize = true;
+            labelY_125.Location = new Point(8, 155);
+            labelY_125.Name = "labelY_125";
+            labelY_125.Size = new Size(54, 25);
+            labelY_125.TabIndex = 15;
+            labelY_125.Text = "+125";
+            labelY_125.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // labelY_0
+            // 
+            labelY_0.AutoSize = true;
+            labelY_0.Location = new Point(40, 279);
+            labelY_0.Name = "labelY_0";
+            labelY_0.Size = new Size(22, 25);
+            labelY_0.TabIndex = 14;
+            labelY_0.Text = "0";
+            labelY_0.TextAlign = ContentAlignment.MiddleCenter;
+            // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(560, 947);
+            ClientSize = new Size(601, 947);
+            Controls.Add(labelY__250);
+            Controls.Add(labelY__125);
+            Controls.Add(labelY_250);
+            Controls.Add(labelY_125);
+            Controls.Add(labelY_0);
+            Controls.Add(labelX__250);
+            Controls.Add(labelX__125);
+            Controls.Add(labelX_250);
+            Controls.Add(labelX_125);
+            Controls.Add(labelX_0);
+            Controls.Add(checkBox2);
             Controls.Add(checkBox1);
             Controls.Add(trackBar2);
             Controls.Add(trackBar1);
@@ -153,5 +289,16 @@
         private TrackBar trackBar1;
         private TrackBar trackBar2;
         private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private Label labelX_0;
+        private Label labelX_125;
+        private Label labelX_250;
+        private Label labelX__125;
+        private Label labelX__250;
+        private Label labelY__250;
+        private Label labelY__125;
+        private Label labelY_250;
+        private Label labelY_125;
+        private Label labelY_0;
     }
 }
--- 9.6.2023 (dz)/MainForm.cs
+++ 9.6.2023 (dz)/MainForm.cs
@@ -36,7 +36,32 @@ namespace _9._6._2023__dz_
 
             g.Clear(BackColor);
 
-            await Task.Run(() => {
+            if (checkBox2.Checked == true)
+            {
+                Pen cp = new Pen(Color.LightGray, 2);
+
+                for (int i = 1; i < 10; i++)
+                {
+                    int c = i * 50 + 1;
+                    g.DrawLine(cp, 0, c, 500, c);
+                    g.DrawLine(cp, c, 0, c, 500);
+                }
+                cp.Dispose();
+            }
+
+            if (checkBox1.Checked == true)
+            {
+                g.DrawLine(pen, 0, 252, 500, 252);
+                g.DrawLine(pen, 252, 0, 252, 500);
+            }
+
+            labelY__250.Text = labelX__250.Text = Math.Round(-250f / tb1, 1).ToString();
+            labelY__125.Text = labelX__125.Text = Math.Round(-125f / tb1, 1).ToString();
+            labelY_125.Text = labelX_125.Text = '+' + Math.Round(125f / tb1, 1).ToString();
+            labelY_250.Text = labelX_250.Text = '+' + Math.Round(250f / tb1, 1).ToString();
+
+            await Task.Run(() =>
+            {
                 isDrawing = true;
 
                 try
@@ -60,7 +85,8 @@ namespace _9._6._2023__dz_
                 }
                 catch (Exception ex)
                 {
-                    Invoke((Action)(() => {
+                    Invoke((Action)(() =>
+                    {
                         label2.Text = ex.Message;
                     }));
                 }
